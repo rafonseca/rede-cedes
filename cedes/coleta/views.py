a43a5c062976bb22916885835a59f27f98604233
rafonseca <renanfonseca@gmail.com>
2018-02-26 17:54:24 -0300

    Pep8 compliance

--- cedes/coleta/views.py
+++ cedes/coleta/views.py
@@ -11,46 +11,57 @@ from django.http import HttpResponseRedirect
 def index(request):
     return render(request,'coleta/index_geral.html')
 
-def index_centro(request,centro):
+
+def index_centro(request, centro):
     # print(request,centro)
-    centro_object=CentroPesquisa.objects.get(uf=centro)
-    lista1=['1a','1b','1c']
-    lista2=['2a','2b','2c']
-    lista3=['3a','3b','3c']
-    lista_de_listas=[lista1,lista2,lista3]
+    centro_object = CentroPesquisa.objects.get(uf=centro)
+    lista1 = ['1a', '1b', '1c']
+    lista2 = ['2a', '2b', '2c']
+    lista3 = ['3a', '3b', '3c']
+    lista_de_listas = [lista1, lista2, lista3]
 
-    return render(request,'coleta/index_centro.html',
-        context={
-            'centro':centro_object.uf,
-            'nome_estado':centro_object.nome_estado,
-            'nome_universidade':centro_object.ies,
-            'jogo_facil':lista1,
-            'jogo_dificil':lista_de_listas
-            })
+    return render(request, 'coleta/index_centro.html',
+                  context={
+                      'centro': centro_object.uf,
+                      'nome_estado': centro_object.nome_estado,
+                      'nome_universidade': centro_object.ies,
+                      'jogo_facil': lista1,
+                      'jogo_dificil': lista_de_listas,
+                      })
 
-class ColetaUpdateView(UserPassesTestMixin,UpdateView):
-    slug_field='centro'
-    slug_url_kwarg='centro'
+
+class ColetaUpdateView(UserPassesTestMixin, UpdateView):
+    slug_field = 'centro'
+    slug_url_kwarg = 'centro'
+
     def test_func(self):
-        # self.success_url = reverse_lazy('index-centro',kwargs={'centro':self.kwargs['centro']})
-        self.redirect_field_name=None
-        obj=self.get_object()
-        gestores=obj.centro.gestores.all()
+        # self.success_url = reverse_lazy('index-centro',
+        # kwargs={'centro':self.kwargs['centro']})
+        self.redirect_field_name = None
+        obj = self.get_object()
+        gestores = obj.centro.gestores.all()
         return self.request.user in gestores
-class ColetaCreateView(UserPassesTestMixin,CreateView):
-    slug_field='centro'
-    slug_url_kwarg='centro'
+
+
+class ColetaCreateView(UserPassesTestMixin, CreateView):
+    slug_field = 'centro'
+    slug_url_kwarg = 'centro'
+
     def test_func(self):
-        centro=self.kwargs['centro']
-        obj=CentroPesquisa.objects.get(uf=centro)
-        gestores=obj.gestores.all()
+        centro = self.kwargs['centro']
+        obj = CentroPesquisa.objects.get(uf=centro)
+        gestores = obj.gestores.all()
         return self.request.user in gestores
+
     def form_valid(self, form):
         self.object = form.save(commit=False)
-        centro=self.kwargs['centro']
-        self.object.centro=CentroPesquisa.objects.get(uf=centro)
+        centro = self.kwargs['centro']
+        self.object.centro = CentroPesquisa.objects.get(uf=centro)
         self.object.save()
-        return HttpResponseRedirect(reverse('pesquisa-list',kwargs={'centro':centro}))
+        return HttpResponseRedirect(reverse('pesquisa-list',
+                                            kwargs={'centro': centro}))
+
+
 class ColetaListView(ListView):
     def get_queryset(self):
         return super().get_queryset().filter(centro=self.kwargs['centro'])
@@ -58,7 +69,7 @@ class ColetaListView(ListView):
 
 class EstruturaFisicaUpdate(ColetaUpdateView):
     model = EstruturaFisica
-    fields= [
+    fields = [
             'tem_sede',
             'tem_banner',
             'tem_internet',
@@ -72,9 +83,10 @@ class EstruturaFisicaUpdate(ColetaUpdateView):
             'repr_social',
             ]
 
+
 class CentroMemoriaUpdate(ColetaUpdateView):
     model = CentroMemoria
-    fields= [
+    fields = [
             'coordenador',
             'situacao_implementacao',
             'situacao_acervo_fisico',
@@ -88,19 +100,26 @@ class CentroMemoriaUpdate(ColetaUpdateView):
 
 class PesquisaCreate(ColetaCreateView):
     model = Pesquisa
-    fields = ['nome','linha','grupo_pesquisa']
+    fields = ['nome', 'linha', 'grupo_pesquisa']
 
 
 class PesquisaUpdate(ColetaUpdateView):
     model = Pesquisa
-    fields = ['nome','linha','grupo_pesquisa']
+    fields = ['nome', 'linha', 'grupo_pesquisa']
+
+
 class PesquisaList(ColetaListView):
     model = Pesquisa
+
+
 class PesquisaDetail(DetailView):
     model = Pesquisa
+
+
 class PesquisaDelete(DeleteView):
     model = Pesquisa
     success_url = reverse_lazy('pesquisa-list')
+
 
 class EventoCreate(ColetaCreateView):
     model = Evento
@@ -121,6 +140,8 @@ class EventoCreate(ColetaCreateView):
         'bolsistas_envolvidos',
     ]
     success_url = reverse_lazy('evento-list')
+
+
 class EventoUpdate(UpdateView):
     model = Evento
     fields = [
@@ -140,12 +161,19 @@ class EventoUpdate(UpdateView):
         'bolsistas_envolvidos',
     ]
     success_url = reverse_lazy('evento-list')
+
+
 class EventoList(ColetaListView):
     model = Evento
+
+
 class EventoDelete(DeleteView):
     model = Evento
+
+
 class EventoDetail(DetailView):
     model = Evento
+
 
 class PublicacaoCreate(ColetaCreateView):
     model = Publicacao
@@ -158,6 +186,8 @@ class PublicacaoCreate(ColetaCreateView):
         'localizacao_digital',
     ]
     success_url = reverse_lazy('publicacao-list')
+
+
 class PublicacaoUpdate(UpdateView):
     model = Publicacao
     fields = [
@@ -168,13 +198,20 @@ class PublicacaoUpdate(UpdateView):
         'referencia_abnt',
         'localizacao_digital',
     ]
+
+
 class PublicacaoList(ColetaListView):
     model = Publicacao
+
+
 class PublicacaoDetail(DetailView):
     model = Publicacao
+
+
 class PublicacaoDelete(DeleteView):
     model = Publicacao
     success_url = reverse_lazy('publicacao-list')
+
 
 class DifusaoCreate(ColetaCreateView):
     model = DifusaoMidiatica
@@ -188,6 +225,8 @@ class DifusaoCreate(ColetaCreateView):
         'publico_alvo',
     ]
     success_url = reverse_lazy('difusao-list')
+
+
 class DifusaoUpdate(UpdateView):
     model = DifusaoMidiatica
     fields = [
@@ -199,13 +238,20 @@ class DifusaoUpdate(UpdateView):
         'bolsistas_envolvidos',
         'publico_alvo',
     ]
+
+
 class DifusaoList(ColetaListView):
     model = DifusaoMidiatica
+
+
 class DifusaoDelete(DeleteView):
     model = DifusaoMidiatica
     success_url = reverse_lazy('difusao-list')
+
+
 class DifusaoDetail(DetailView):
     model = DifusaoMidiatica
+
 
 class FormacaoCreate(ColetaCreateView):
     model = AtividadeFormacao
@@ -226,6 +272,8 @@ class FormacaoCreate(ColetaCreateView):
         'bolsistas_envolvidos',
     ]
     success_url = reverse_lazy('formacao-list')
+
+
 class FormacaoUpdate(UpdateView):
     model = AtividadeFormacao
     fields = [
@@ -244,11 +292,17 @@ class FormacaoUpdate(UpdateView):
         'pesquisadores_envolvidos',
         'bolsistas_envolvidos',
     ]
+
+
 class FormacaoList(ColetaListView):
     model = AtividadeFormacao
+
+
 class FormacaoDelete(DeleteView):
     model = AtividadeFormacao
     success_url = reverse_lazy('formacao-list')
+
+
 class FormacaoDetail(DetailView):
     model = AtividadeFormacao
 
@@ -265,6 +319,8 @@ class OrientacaoCreate(ColetaCreateView):
         'descricao',
     ]
     success_url = reverse_lazy('orientacao-list')
+
+
 class OrientacaoUpdate(UpdateView):
     model = Orientacao
     fields = [
@@ -276,13 +332,20 @@ class OrientacaoUpdate(UpdateView):
         'autor',
         'descricao',
     ]
+
+
 class OrientacaoList(ColetaListView):
     model = Orientacao
+
+
 class OrientacaoDelete(DeleteView):
     model = Orientacao
     success_url = reverse_lazy('orientacao-list')
+
+
 class OrientacaoDetail(DetailView):
     model = Orientacao
+
 
 class IntercambioCreate(ColetaCreateView):
     model = Intercambio
@@ -297,6 +360,8 @@ class IntercambioCreate(ColetaCreateView):
         'descricao',
     ]
     success_url = reverse_lazy('intercambio-list')
+
+
 class IntercambioUpdate(UpdateView):
     model = Intercambio
     fields = [
@@ -309,13 +374,20 @@ class IntercambioUpdate(UpdateView):
         'grupos_estudo',
         'descricao',
     ]
+
+
 class IntercambioList(ColetaListView):
     model = Intercambio
+
+
 class IntercambioDelete(DeleteView):
     model = Intercambio
     success_url = reverse_lazy('intercambio-list')
+
+
 class IntercambioDetail(DetailView):
     model = Intercambio
+
 
 class IntervencaoCreate(ColetaCreateView):
     model = IntervencaoPolitica
@@ -332,6 +404,8 @@ class IntervencaoCreate(ColetaCreateView):
         'bolsistas_envolvidos',
     ]
     success_url = reverse_lazy('intervencao-list')
+
+
 class IntervencaoUpdate(UpdateView):
     model = IntervencaoPolitica
     fields = [
@@ -346,10 +420,16 @@ class IntervencaoUpdate(UpdateView):
         'pesquisadores_envolvidos',
         'bolsistas_envolvidos',
     ]
+
+
 class IntervencaoList(ColetaListView):
     model = IntervencaoPolitica
+
+
 class IntervencaoDelete(DeleteView):
     model = IntervencaoPolitica
     success_url = reverse_lazy('intervencao-list')
+
+
 class IntervencaoDetail(DetailView):
     model = IntervencaoPolitica
